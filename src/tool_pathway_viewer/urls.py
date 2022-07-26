3ff3442ab0faf3aa88636b04db0f59ae5d3793ee
Anders Riutta <git@andersriutta.com>
2022-07-26 15:46:08 -0700

    update docs

--- src/tool_pathway_viewer/urls.py
+++ src/tool_pathway_viewer/urls.py
@@ -17,13 +17,28 @@ from django.contrib import admin
 from django.urls import include, path
 from redirect import views
 
+#############
+# URL Formats
+#############
+
+# OLD
 # https://www.wikipathways.org/wpi/PathwayWidget.php?id=WP528&xref[]=10400,Entrez Gene&xref[]=62-49-7,CAS&xref[]=1119,Entrez Gene&colors=green,red,blue
+
+# NEW
 # https://pathway-viewer.toolforge.org/?id=WP528&lightgreen=Entrez_Gene_10400&pink=CAS_62-49-7&lightblue=Entrez_Gene_1119
 
+# ALL REDIRECT TO
+# https://pathway-viewer.toolforge.org/embed/WP528&lightgreen=Entrez_Gene_10400&pink=CAS_62-49-7&lightblue=Entrez_Gene_1119
+
 urlpatterns = [
+    # views.index_and_old_toolforge_url in src/redirect/views.py
     path('', views.index_and_old_toolforge_url),
     path('PathwayWidget.php', views.old_pathway_widget_php),
+    path('wpi/PathwayWidget.php', views.old_pathway_widget_php),
+    # embed.urls in src/embed/urls.py
     path('embed/', include('embed.urls')),
+    # views.simolecule in src/redirect/views.py
     path('simolecule/', views.simolecule, name='simolecule'),
+    # not really using admin
     path('admin/', admin.site.urls),
 ]
