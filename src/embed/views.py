9bd522a17359db86da02d79e0756751c8d2716c6
Anders Riutta <git@andersriutta.com>
2022-07-26 14:40:40 -0700

    Pull SVG from GH instead of local

--- src/embed/views.py
+++ src/embed/views.py
@@ -37,44 +37,43 @@ def embed(request, wpid):
     if not WPRE.fullmatch(wpid):
         return HttpResponse('<html><body><p>Invalid WikiPathways ID. Must must be of format WP + number, e.g., WP554</p></body></html>')
       
-    # TODO: should we convert the old revisions and then support the "rev" query param?
-    svg_f = Path(SVG_DIR + "/" + wpid + theme_filename_part + '.svg')
+    # TODO: should we convert the old revisions to git hashes and then support the "rev" query param?
+    svg_url = f'https://raw.githubusercontent.com/wikipathways/wikipathways-assets/main/pathways/{wpid}/{wpid}.svg'
+    svg_res = requests.get(svg_url)
+    if svg_res.status_code != 200:
+        return HttpResponse(f'<html><body><p>No SVG available for {svg_url}</p></body></html>')
 
-    if not svg_f.exists():
-        return HttpResponse('<html><body><p>No SVG available for ' + wpid + '</p></body></html>')
+    svg_data = svg_res.text
+    highlight_style = ''
+    for key, value in request.GET.items():
+        validated_color = ""
+        if key in CSS3_NAMES_TO_HEX:
+            validated_color = key
+        else:
+            # if prefix '#' is present, remove it for consistency. We'll add it back later.
+            if key[0] == '#':
+                key = key[1:]
 
-    with svg_f.open() as file:
-        svg_data = file.read()
-        highlight_style = ''
-        for key, value in request.GET.items():
-            validated_color = ""
-            if key in CSS3_NAMES_TO_HEX:
-                validated_color = key
-            else:
-                # if prefix '#' is present, remove it for consistency. We'll add it back later.
-                if key[0] == '#':
-                    key = key[1:]
+            # valid lengths for color hex codes: 3, 6 or 8
+            if (len(key) in [3, 6, 8]) and (HEXADECIMAL_RE.fullmatch(key) is not None):
+                validated_color = "#" + key
 
-                # valid lengths for color hex codes: 3, 6 or 8
-                if (len(key) in [3, 6, 8]) and (HEXADECIMAL_RE.fullmatch(key) is not None):
-                    validated_color = "#" + key
-    
-            if validated_color != "":
-                for raw_selector in value.split(","):
-                    # The sub and find steps are to guard against malicious input.
-                    # If the selector, with any characters other than alphanumeric
-                    # or dash converted to unscores, are not already in the SVG,
-                    # we don't include that selector.
+        if validated_color != "":
+            for raw_selector in value.split(","):
+                # The sub and find steps are to guard against malicious input.
+                # If the selector, with any characters other than alphanumeric
+                # or dash converted to unscores, are not already in the SVG,
+                # we don't include that selector.
 
-                    selector = NON_ALPHANUMERIC_DASH_CHAR_RE.sub("_", raw_selector)
+                selector = NON_ALPHANUMERIC_DASH_CHAR_RE.sub("_", raw_selector)
 
-                    if svg_data.find(selector) > -1:
-                        highlight_style += ('#' + selector + ' .Icon, .' + selector + ' .Icon, [name="' + selector + '"] .Icon { fill: ' + validated_color + ';}\n')
-                        highlight_style += ('#' + selector + '.Edge path, .' + selector + '.Edge path { stroke: ' + validated_color + ';}\n')
+                if svg_data.find(selector) > -1:
+                    highlight_style += ('#' + selector + ' .Icon, .' + selector + ' .Icon, [name="' + selector + '"] .Icon { fill: ' + validated_color + ';}\n')
+                    highlight_style += ('#' + selector + '.Edge path, .' + selector + '.Edge path { stroke: ' + validated_color + ';}\n')
 
-        return render(
-                request, "embed.html", {"highlight_style": highlight_style, "svg_data": svg_data}
-        )
+    return render(
+            request, "embed.html", {"highlight_style": highlight_style, "svg_data": svg_data}
+    )
 
 def help(request):
     return render(
